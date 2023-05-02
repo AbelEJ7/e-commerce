d69e549f56c91862bf2d7ce417d988c25f75ca13
Abel Jembere <113380870+AbelEJ7@users.noreply.github.com>
2023-05-02 13:58:00 +0200

    initialized redux and migrated context of user and category to redux

---
+++ src/store/categories/category.selector.js
@@ -0,0 +1,18 @@
+import {createSelector} from 'reselect';
+
+const selectCategoryReducer= (state) => state.categories;
+
+export const SelectCategories =createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+
+export const selectCategoriesMap = createSelector(
+  [SelectCategories],
+  (categories) => categories.reduce((acc, category) => {
+    const { title, items } = category;
+    acc[title.toLowerCase()] = items
+    console.log('cat fies')
+    return acc;
+  }, {}));
